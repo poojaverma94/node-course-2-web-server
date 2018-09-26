09acdf6ec9ee7ab571cda6b59d68119675e84c12
pooja verma <poojaverma2094@gmail.com>
2018-09-25 20:28:53 -1100

    Add project page

--- server.js
+++ server.js
@@ -60,6 +60,12 @@ app.get('/about' , (req,res) => {
  });
 });
 
+app.get('/projects' , (req,res) => {
+  res.render('projects.hbs' , {
+    pageTitle: 'projects'
+  });
+});
+
 app.get('/bad', (req, res) =>
 {
   res.send({
